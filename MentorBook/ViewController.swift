d58e6b8a2b285a32e433a34662ae2e0d90870209
watanabe nami <210392v@ugs.kochi-tech.ac.jp>
2020-09-20 16:37:23 +0900

    メンター図鑑完成

--- MentorBook/ViewController.swift
+++ MentorBook/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mentorArray: [Mentor] = []
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    
+    //表示するメンターの番号
+    var index: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mentorArray.append(Mentor(name: "ながた", imageName: "nagata.jpg", course: "iPhone"))
+        mentorArray.append(Mentor(name: "りょう", imageName: "ryo.jpg", course: "Unity"))
+        mentorArray.append(Mentor(name: "たいてぃ", imageName: "taithi.jpg", course: "WebS, WebD"))
+        
+        setUI()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func setUI(){
+        if(index < 0){
+            index = 0
+        }else if(2 < index){
+            index = 2
+        }
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabel.text = mentorArray[index].course
     }
 
+    @IBAction func back(){
+        index -= 1
+        setUI()
+    }
+    
+    @IBAction func next(){
+        index += 1
+        setUI()
+        
+    }
 
 }
 
